e7ce987ea3e17f3af52a2842c5ca71d45a72db00
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-09-13 13:22:26 +0900

    Remove -Wno-implicit-function-declaration

--- _dummy/sys/stat.h
+++ _dummy/sys/stat.h
@@ -8,4 +8,13 @@ struct stat {
 	mode_t st_mode;
 };
 
+#define S_ISREG(m)  0
+#define S_ISDIR(m)  0
+#define S_ISBLK(m)  0
+#define S_ISCHR(m)  0
+#define S_ISFIFO(m) 0
+
+FILE *fopen(const char *path, const char *mode);
+int stat(const char *pathname, struct stat *buf);
+
 #endif /* !_DUMMY_STAT_H_ */
